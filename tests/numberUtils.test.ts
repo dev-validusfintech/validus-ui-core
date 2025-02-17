b55ded8a7562099ace4d5022aa3e953fefa9acbd
nrupa-validus <v-nrupa.s@validusfintech.com>
2025-02-17 11:55:59 +0530

    single digit decimals

--- tests/numberUtils.test.ts
+++ tests/numberUtils.test.ts
@@ -58,4 +58,17 @@ test("converts numbers to words", () => {
   expect(amountInWords(123456789.75)).toBe(
     "Rupees Twelve Crore Thirty Four Lakh Fifty Six Thousand Seven Hundred Eighty Nine and Seventy Five Paise",
   );
+  expect(amountInWords(8407970809.68)).toBe('Rupees Eight Hundred Forty Crore Seventy Nine Lakh Seventy Thousand Eight Hundred Nine and Sixty Eight Paise');
+  expect(amountInWords(6649900710.8)).toBe('Rupees Six Hundred Sixty Four Crore Ninety Nine Lakh Seven Hundred Ten and Eighty Paise');
+  expect(amountInWords(4399212265.11)).toBe('Rupees Four Hundred Thirty Nine Crore Ninety Two Lakh Twelve Thousand Two Hundred Sixty Five and Eleven Paise');
+  expect(amountInWords(5690654047.65)).toBe('Rupees Five Hundred Sixty Nine Crore Six Lakh Fifty Four Thousand Forty Seven and Sixty Five Paise');
+  expect(amountInWords(6038710079.18)).toBe('Rupees Six Hundred Three Crore Eighty Seven Lakh Ten Thousand Seventy Nine and Eighteen Paise');
+  expect(amountInWords(3805289233.0)).toBe('Rupees Three Hundred Eighty Crore Fifty Two Lakh Eighty Nine Thousand Two Hundred Thirty Three');
+  expect(amountInWords(4981957958.1)).toBe('Rupees Four Hundred Ninety Eight Crore Nineteen Lakh Fifty Seven Thousand Nine Hundred Fifty Eight and Ten Paise');
+  expect(amountInWords(3119801006.14)).toBe('Rupees Three Hundred Eleven Crore Ninety Eight Lakh One Thousand Six and Fourteen Paise');
+  expect(amountInWords(5523577145.54)).toBe('Rupees Five Hundred Fifty Two Crore Thirty Five Lakh Seventy Seven Thousand One Hundred Forty Five and Fifty Four Paise');
+  expect(amountInWords(1887252642.27)).toBe('Rupees One Hundred Eighty Eight Crore Seventy Two Lakh Fifty Two Thousand Six Hundred Forty Two and Twenty Seven Paise');
+  expect(amountInWords(7694495757.07)).toBe('Rupees Seven Hundred Sixty Nine Crore Forty Four Lakh Ninety Five Thousand Seven Hundred Fifty Seven and Seven Paise');
+  expect(amountInWords(5088085809.15)).toBe('Rupees Five Hundred Eight Crore Eighty Lakh Eighty Five Thousand Eight Hundred Nine and Fifteen Paise');
+  expect(amountInWords(6514938353.8)).toBe('Rupees Six Hundred Fifty One Crore Forty Nine Lakh Thirty Eight Thousand Three Hundred Fifty Three and Eighty Paise');
 });
